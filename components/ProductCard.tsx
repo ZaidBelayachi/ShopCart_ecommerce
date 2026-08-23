8b53f3f237da45c9c4ce6a63c7dd62ca8597f79a
ZaidBelayachi <zaid.belayachii@gmail.com>
2026-08-23 16:37:50 +0100

    seted up product single page with dynamic routing and Sanity data fetching

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,62 +1,70 @@
-import { Product } from '@/sanity.types'
-import { urlFor } from '@/sanity/lib/image'
-import { Flame, Link, StarIcon } from 'lucide-react'
-import Image from 'next/image'
-import React from 'react'
-import AddToWishlistButton from './AddToWishlistButton'
-import { Title } from './ui/text'
-import PriceView from './PriceView'
-import AddToCartButton from './AddToCartButton'
+import { Product } from "@/sanity.types";
+import { urlFor } from "@/sanity/lib/image";
+import Image from "next/image";
+import React from "react";
+import Link from "next/link";
+import { StarIcon } from "@sanity/icons";
+import { Flame } from "lucide-react";
+import PriceView from "./PriceView";
+import Title from "./Title";
+import ProductSideMenu from "./ProductSideMenu";
+import AddToCartButton from "./AddToCartButton";
 
-
-const ProductCard = ({product}:{product:Product}) => {
+const ProductCard = ({ product }: { product: Product }) => {
   return (
-    <div className='text-sm border-[1px] border-dark_blue/20 rounded-md bg-white group'>
-        <div className='relative group overflow-hidden bg-shop_light_bg'>
-            {product?.images && (
+    <div className="text-sm border-[1px] rounded-md border-darkBlue/20 group bg-white">
+      <div className="relative group overflow-hidden bg-shop_light_bg">
+        {product?.images && (
+          <Link href={`/product/${product?.slug?.current}`}>
             <Image
-             src={urlFor(product?.images[0]).url()}  
-             alt="ProductImage"
-             loading='lazy' 
-             width={700} 
-             height={700}
-             className={`w-full h-64 object-contain overflow-hidden transition-transform bg-shop_light_bg hoverEffect 
+              src={urlFor(product.images[0]).url()}
+              alt="productImage"
+              width={500}
+              height={500}
+              priority
+              className={`w-full h-64 object-contain overflow-hidden transition-transform bg-shop_light_bg duration-500 
               ${product?.stock !== 0 ? "group-hover:scale-105" : "opacity-50"}`}
-              />
-             )}
-             <AddToWishlistButton product={product}/>
-             {product?.status === "sale" && (<p className='absolute top-2 left-2 z-10 text-xs border border-darkColor/50 px-2 rounded-full group-hover:border-shop_light_green group-hover:text-shop_light_green hoverEffect'>Sale!</p>
-            )}
-            {product?.status === "hot" && <Link 
+            />
+          </Link>
+        )}
+        <ProductSideMenu product={product} />
+        {product?.status === "sale" ? (
+          <p className="absolute top-2 left-2 z-10 text-xs border border-darkColor/50 px-2 rounded-full group-hover:border-lightGreen hover:text-shop_dark_green hoverEffect">
+            Sale!
+          </p>
+        ) : (
+          <Link
             href={"/deal"}
-            className="absolute top-2 left-2 z-10 border border-shop_orange/50 p-1 rounded-full group-hover:border-shop_orange hover:text-shop_dark_green hoverEffect">
-            <Flame 
+            className="absolute top-2 left-2 z-10 border border-shop_orange/50 p-1 rounded-full group-hover:border-shop_orange hover:text-shop_dark_green hoverEffect"
+          >
+            <Flame
               size={18}
               fill="#fb6c08"
               className="text-shop_orange/50 group-hover:text-shop_orange hoverEffect"
             />
-            </Link>}
-        </div>
-        <div className='p-3'>
-            {product?.categories && ( <p className='uppercase line-clamp-1 text-xs text-shop_light_text'>{product?.categories?.map((cat)=>cat).join(", ")}</p>
+          </Link>
         )}
-        <Title className='text-sm line-clamp-1 '>
-            {product?.name}
-        </Title>
-             <div className="flex items-center gap-2">
+      </div>
+      <div className="p-3 flex flex-col gap-2">
+        {product?.categories && (
+          <p className="uppercase line-clamp-1 text-xs font-medium text-lightText">
+            {product.categories.map((cat) => cat).join(", ")}
+          </p>
+        )}
+        <Title className="text-sm line-clamp-1">{product?.name}</Title>
+        <div className="flex items-center gap-2">
           <div className="flex items-center">
             {[...Array(5)].map((_, index) => (
               <StarIcon
-              size={12}
                 key={index}
                 className={
-                  index < 4 ? "text-shop_lighter_green" : " text-shop_lighter_text"
+                  index < 4 ? "text-shop_light_green" : " text-lightText"
                 }
                 fill={index < 4 ? "#93D991" : "#ababab"}
               />
             ))}
           </div>
-          <p className="text-shop_light_text text-xs tracking-wide">5 Reviews</p>
+          <p className="text-lightText text-xs tracking-wide">5 Reviews</p>
         </div>
 
         <div className="flex items-center gap-2.5">
@@ -67,16 +75,16 @@ const ProductCard = ({product}:{product:Product}) => {
             {(product?.stock as number) > 0 ? product?.stock : "unavailable"}
           </p>
         </div>
+
         <PriceView
           price={product?.price}
           discount={product?.discount}
           className="text-sm"
         />
-        <AddToCartButton product={product} className="w-32 rounded-full"/>
-        
-        </div>
+        <AddToCartButton product={product} className="w-36 rounded-full" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProductCard
+export default ProductCard;
