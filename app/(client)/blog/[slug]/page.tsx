640a863c08c15f7f0f506e3fa265a8f7d6b3b1c5
ZaidBelayachi <zaid.belayachii@gmail.com>
2026-08-28 17:01:02 +0100

    implemented dynamic shop page with category, brand, and price filtering

--- app/(client)/blog/[slug]/page.tsx
+++ app/(client)/blog/[slug]/page.tsx
@@ -1,9 +1,23 @@
+import Container from '@/components/Container';
+import Title from '@/components/Title';
 import React from 'react'
 
-const BlogPage = () => {
+const SingleBlogPage = async({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) => {
+  const { slug } = await params;
   return (
-    <div>BlogPage</div>
+    <div>
+      <Container>
+        <Title>
+          Single Blog Page 
+        </Title>
+        <p>{slug}</p>
+      </Container>
+    </div>
   )
 }
 
-export default BlogPage
+export default SingleBlogPage
